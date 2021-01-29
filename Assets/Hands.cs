6b553c29ff4f66f7895d27e948790e49a4307f67
nick0124 <nikita.popov@lindenvalley.de>
2021-01-30 01:23:04 +0500

    Added grab and throw objects

--- Assets/Hands.cs
+++ Assets/Hands.cs
@@ -5,19 +5,73 @@ using UnityEngine;
 public class Hands : MonoBehaviour {
 	public Transform cam;
 	public Transform hands;
+	public FixedJoint handsJoint;
+	public Transform hitTransform;
+	public Rigidbody hitRb;
+
+	public float grabDistance;
 
 	// Start is called before the first frame update
 	void Start() {
-
+		
 	}
 
 	// Update is called once per frame
 	void Update() {
-
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("grab");
+			if(hitRb != null) {
+				handsJoint.connectedBody = hitRb;
+			}
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			Debug.Log("drop");
+			if (handsJoint.connectedBody != null) {
+				handsJoint.connectedBody.GetComponent<Rigidbody>().AddForce(Vector3.zero, ForceMode.Impulse);
+			}
+			handsJoint.connectedBody = null;
+		}
+		if (Input.GetMouseButtonUp(1)) {
+			Debug.Log("throw");
+			if (handsJoint.connectedBody != null) {
+				handsJoint.connectedBody.GetComponent<Rigidbody>().AddForce(cam.forward*10, ForceMode.Impulse);
+			}
+			handsJoint.connectedBody = null;
+		}
 	}
 
 	void FixedUpdate() {
 		hands.transform.localPosition = new Vector3(0, 0, 1);
+
+		Debug.DrawRay(cam.position, cam.forward, Color.green);
+
+		RaycastHit hit;
+		Ray ray = new Ray(cam.position, cam.forward);
+
+		if (Physics.Raycast(ray, out hit)) {
+			if (hit.distance < grabDistance) {
+				hitTransform = hit.transform;
+				if (hitTransform.GetComponent<Rigidbody>() == null) {
+					hitTransform = null;
+					hitRb = null;
+				} else {
+					if (hitTransform.GetComponent<Rigidbody>().isKinematic) {
+						hitTransform = null;
+						hitRb = null;
+					} else {
+						hitRb = hitTransform.GetComponent<Rigidbody>();
+					}
+				}
+			} else {
+				hitTransform = null;
+				hitRb = null;
+			}
+
+			// Do something with the object that was hit by the raycast.
+		} else {
+			hitTransform = null;
+			hitRb = null;
+		}
 	}
 
 	private void OnTriggerEnter(Collider other) {
